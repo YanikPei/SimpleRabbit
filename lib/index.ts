eb240dab74888bd33f0127bf09e9a4fd49c15f8d
yanikpeiffer <yanikpeiffer@gmail.com>
2019-08-02 14:55:54 +0200

    modify lib for npm

--- lib/index.ts
+++ lib/index.ts
@@ -1,38 +1,49 @@
 const amqp = require('amqplib');
 import QueueSubscribtion from './QueueSubscription';
-export const queueCon = amqp.connect(process.env.QUEUE_SERVER);
 
-export function initSubscribers(subscribeTo: QueueSubscribtion[]) {
+export * from "./QueuePublisherMessage";
+export * from "./QueueRPCMessage";
+export * from "./QueueSubscription";
 
-    queueCon.then((con) => {
-        con.createChannel().then((ch) => {
+export class QueueConnection {
+    queueCon;
 
-            subscribeTo.forEach((sub) => {
+    constructor(queueServer: string) {
+        this.queueCon = amqp.connect(process.env.QUEUE_SERVER);
+    }
 
-                ch.assertExchange(sub.exchange, 'topic', { durable: false })
-                    .then(() => {
-                        return ch.assertQueue(sub.topic, { autoDelete: true });
-                    })
-                    .then((qok) => {
-                        ch.bindQueue(qok.queue, sub.exchange, sub.topic);
-                        return qok.queue;
-                    })
-                    .then((queue) => {
-                        return ch.consume(queue, (msg) => {
+    initSubscribers(subscribeTo: QueueSubscribtion[]) {
 
-                            sub.func(JSON.parse(msg.content.toString()))
-                                .then(res => {
-                                    if(msg.properties.replyTo) {
-                                        ch.sendToQueue(msg.properties.replyTo, new Buffer(JSON.stringify(res)), {
-                                            correlationId: msg.properties.correlationId
-                                        });
-                                    }
-                                });
-                        }, { noAck: true });
-                    })
+        this.queueCon.then((con) => {
+            con.createChannel().then((ch) => {
+    
+                subscribeTo.forEach((sub) => {
+    
+                    ch.assertExchange(sub.exchange, 'topic', { durable: false })
+                        .then(() => {
+                            return ch.assertQueue(sub.topic, { autoDelete: true });
+                        })
+                        .then((qok) => {
+                            ch.bindQueue(qok.queue, sub.exchange, sub.topic);
+                            return qok.queue;
+                        })
+                        .then((queue) => {
+                            return ch.consume(queue, (msg) => {
+    
+                                sub.func(JSON.parse(msg.content.toString()))
+                                    .then(res => {
+                                        if(msg.properties.replyTo) {
+                                            ch.sendToQueue(msg.properties.replyTo, new Buffer(JSON.stringify(res)), {
+                                                correlationId: msg.properties.correlationId
+                                            });
+                                        }
+                                    });
+                            }, { noAck: true });
+                        })
+                })
+    
             })
-
-        })
-    });
-
-}
+        });
+    
+    }
+};
