fd7a3a4c0508368df00025fc9e351a88f306b36b
yanikpeiffer <yanikpeiffer@gmail.com>
2019-10-17 17:59:30 +0200

    fix race condition when channel is closing

--- lib/QueueClient.ts
+++ lib/QueueClient.ts
@@ -119,11 +119,11 @@ export class QueueClient {
         );
 
         // listen for callback
-        return await channel.consume(queue.queue, (msg) => {
+        return await channel.consume(queue.queue, async (msg) => {
             if (msg.properties.correlationId == correlationID) {
                 const msgJson = JSON.parse(msg.content.toString());
-                channel.deleteQueue(queue.queue);
-                channel.close();
+                await channel.deleteQueue(queue.queue);
+                await channel.close();
 
                 callback(msgJson);
             }
